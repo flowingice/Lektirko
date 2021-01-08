df44a9348bd1dcf02ab63df0d79220a6627de522
Mate Misic <mate.misic@s2is.hr>
2021-01-08 22:36:05 +0100

    Initial commit

---
+++ app/src/main/java/hr/tvz/mmisic/lektirko/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.tvz.mmisic.lektirko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
